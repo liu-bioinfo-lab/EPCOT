1a52f55c321c3b78ee19f3ef3da7f0c94939ff47
张振豪 <zhangzhenhao@gous-MacBook-Pro-2.local>
2022-09-13 02:20:14 -0400

    eap

--- EAP/layers.py
+++ EAP/layers.py
@@ -6,15 +6,12 @@ from einops import rearrange, reduce
 from einops.layers.torch import Rearrange
 from torch import nn, einsum
 class CNN(nn.Module):
-    def __init__(self, nclass, seq_length,embed_length):
+    def __init__(self):
         super(CNN, self).__init__()
         conv_kernel_size1 = 10
         conv_kernel_size2 = 8
         pool_kernel_size1 = 5
         pool_kernel_size2 = 4
-        sequence_length = seq_length
-        n_targets = nclass
-        linear_size = embed_length
         self.conv_net = nn.Sequential(
             nn.Conv1d(5, 256, kernel_size=conv_kernel_size1),
             nn.ReLU(inplace=True),
@@ -39,51 +36,11 @@ class CNN(nn.Module):
             nn.ReLU(inplace=True),
             nn.BatchNorm1d(512),
             nn.Dropout(p=0.2))
-        reduce_by1 = 2 * (conv_kernel_size1 - 1)
-        reduce_by2 = 2 * (conv_kernel_size2 - 1)
-        pool_kernel_size1 = float(pool_kernel_size1)
-        pool_kernel_size2 = float(pool_kernel_size2)
-        self._n_channels = int(
-            np.floor(
-                (np.floor(
-                    (sequence_length - reduce_by1) / pool_kernel_size1)
-                 - reduce_by2) / pool_kernel_size2)
-            - reduce_by2)
-        self.linear = nn.Linear(512* self._n_channels, linear_size)
-        self.batch_norm = nn.BatchNorm1d(linear_size)
-        self.classifier = nn.Linear(linear_size, n_targets)
-        self.relu = nn.ReLU()
         self.num_channels = 512
     def forward(self, x):
         out = self.conv_net(x)
         return out
 
-
-class Transformer(nn.Module):
-    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout = 0.):
-        super().__init__()
-        self.layers = nn.ModuleList([])
-        for _ in range(depth):
-            self.layers.append(nn.ModuleList([
-                PreNorm(dim, Attention(dim, heads = heads, dim_head = dim_head, dropout = dropout)),
-                PreNorm(dim, FeedForward(dim, mlp_dim, dropout = dropout))
-            ]))
-    def forward(self, x):
-        for attn, ff in self.layers:
-            x = attn(x) + x
-            x = ff(x) + x
-        return x
-
-
-
-class linearRegression(torch.nn.Module):
-    def __init__(self, inputSize, outputSize):
-        super(linearRegression, self).__init__()
-        self.linear = torch.nn.Linear(inputSize, outputSize)
-
-    def forward(self, x):
-        out = self.linear(x)
-        return out
 class AttentionPool(nn.Module):
     def __init__(self, dim):
         super().__init__()
