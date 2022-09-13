e05081a642d01481f5748424db1e68572953b5f1
张振豪 <zhangzhenhao@gous-MacBook-Pro-2.local>
2022-09-12 23:23:52 -0400

    microc

--- COP/microc/model.py
+++ COP/microc/model.py
@@ -4,52 +4,24 @@ import numpy as np
 from torch import nn, Tensor,einsum
 from microc.transformer import Transformer
 from einops import rearrange
-from microc.layers import dilated_tower,AttentionPool,CNN
+from microc.layers import dilated_tower,AttentionPool
 from einops.layers.torch import Rearrange
-class GroupWiseLinear(nn.Module):
-    def __init__(self, num_class, hidden_dim, bias=True):
-        super().__init__()
-        self.num_class = num_class
-        self.hidden_dim = hidden_dim
-        self.bias = bias
-        self.W = nn.Parameter(torch.Tensor(1, num_class, hidden_dim))
-        if bias:
-            self.b = nn.Parameter(torch.Tensor(1, num_class))
-        self.reset_parameters()
-    def reset_parameters(self):
-        stdv = 1. / math.sqrt(self.W.size(2))
-        for i in range(self.num_class):
-            self.W[0][i].data.uniform_(-stdv, stdv)
-        if self.bias:
-            for i in range(self.num_class):
-                self.b[0][i].data.uniform_(-stdv, stdv)
-    def forward(self, x):
-        x = (self.W * x).sum(-1)
-        if self.bias:
-            x = x + self.b
-        return x
+from hic.model import Tranmodel,build_backbone,build_transformer
 
-class Tranmodel(nn.Module):
-    def __init__(self, backbone, transfomer, num_class):
-        super().__init__()
-        self.backbone = backbone
-        self.transformer = transfomer
-        self.num_class = num_class
-        feature_pos=backbone._n_channels
-        hidden_dim = transfomer.d_model
-        self.feature_pos_encoding = nn.Parameter(torch.randn(1, hidden_dim, feature_pos))
-        self.label_input = torch.Tensor(np.arange(num_class)).view(1, -1).long()
-        self.input_proj = nn.Conv1d(backbone.num_channels, hidden_dim, kernel_size=1)
-        self.query_embed = nn.Embedding(num_class, hidden_dim)
-        self.fc = GroupWiseLinear(num_class, hidden_dim, bias=True)
-    def forward(self, input,fea_pos=False):
-        input=rearrange(input,'b n c l -> (b n) c l')
-        src = self.backbone(input)
-        src=self.input_proj(src)
-        if fea_pos:
-            src+=self.feature_pos_encoding
-        src = self.transformer(src)
-        return src
+# class Tranmodel(nn.Module):
+#     def __init__(self, backbone, transfomer, num_class):
+#         super().__init__()
+#         self.backbone = backbone
+#         self.transformer = transfomer
+#         self.num_class = num_class
+#         hidden_dim = transfomer.d_model
+#         self.input_proj = nn.Conv1d(backbone.num_channels, hidden_dim, kernel_size=1)
+#     def forward(self, input):
+#         input=rearrange(input,'b n c l -> (b n) c l')
+#         src = self.backbone(input)
+#         src=self.input_proj(src)
+#         src = self.transformer(src)
+#         return src
 
 
 class finetunemodel(nn.Module):
@@ -148,20 +120,20 @@ class finetunemodel(nn.Module):
         return x
 
 
-def build_backbone(args):
-    model = CNN(args.num_class, args.seq_length, args.embedsize)
-    return model
-def build_transformer(args):
-    return Transformer(
-        d_model=args.hidden_dim,
-        dropout=args.dropout,
-        nhead=args.nheads,
-        dim_feedforward=args.dim_feedforward,
-        num_encoder_layers=args.enc_layers,
-        num_decoder_layers=args.dec_layers
-    )
+# def build_backbone():
+#     model = CNN()
+#     return model
+# def build_transformer(args):
+#     return Transformer(
+#         d_model=args.hidden_dim,
+#         dropout=args.dropout,
+#         nhead=args.nheads,
+#         dim_feedforward=args.dim_feedforward,
+#         num_encoder_layers=args.enc_layers,
+#         num_decoder_layers=args.dec_layers
+#     )
 def build_pretrain_model_microc(args):
-    backbone = build_backbone(args)
+    backbone = build_backbone()
     transformer = build_transformer(args)
     pretrain_model = Tranmodel(
             backbone=backbone,
@@ -170,7 +142,11 @@ def build_pretrain_model_microc(args):
         )
     if args.pretrain_path != 'none':
         print('load pre-training model: '+args.pretrain_path)
-        pretrain_model.load_state_dict(torch.load(args.pretrain_path, map_location='cpu'))
+        model_dict = pretrain_model.state_dict()
+        pretrain_dict = torch.load(args.pretrain_path, map_location='cpu')
+        pretrain_dict = {k: v for k, v in pretrain_dict.items() if k in model_dict}
+        model_dict.update(pretrain_dict)
+        pretrain_model.load_state_dict(model_dict)
     if not args.fine_tune:
         for param in pretrain_model.parameters():
             param.requires_grad = False
