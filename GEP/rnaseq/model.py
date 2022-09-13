f170e7308aabae3f2912bc3bf9342cf678bb6273
张振豪 <zhangzhenhao@gous-MacBook-Pro-2.local>
2022-09-13 02:05:42 -0400

    rna

--- GEP/rnaseq/model.py
+++ GEP/rnaseq/model.py
@@ -6,51 +6,7 @@ from cage.transformer import Transformer
 import torch.nn.functional as F
 from einops import rearrange
 from einops.layers.torch import Rearrange
-class GroupWiseLinear(nn.Module):
-    def __init__(self, num_class, hidden_dim, bias=True):
-        super().__init__()
-        self.num_class = num_class
-        self.hidden_dim = hidden_dim
-        self.bias = bias
-        self.W = nn.Parameter(torch.Tensor(1, num_class, hidden_dim))
-        if bias:
-            self.b = nn.Parameter(torch.Tensor(1, num_class))
-        self.reset_parameters()
-    def reset_parameters(self):
-        stdv = 1. / math.sqrt(self.W.size(2))
-        for i in range(self.num_class):
-            self.W[0][i].data.uniform_(-stdv, stdv)
-        if self.bias:
-            for i in range(self.num_class):
-                self.b[0][i].data.uniform_(-stdv, stdv)
-    def forward(self, x):
-        x = (self.W * x).sum(-1)
-        if self.bias:
-            x = x + self.b
-        return x
-
-class Tranmodel(nn.Module):
-    def __init__(self, backbone, transfomer, num_class):
-        super().__init__()
-        self.backbone = backbone
-        self.transformer = transfomer
-        self.num_class = num_class
-        feature_pos=backbone._n_channels
-        hidden_dim = transfomer.d_model
-        self.feature_pos_encoding = nn.Parameter(torch.randn(1, hidden_dim, feature_pos))
-        self.label_input = torch.Tensor(np.arange(num_class)).view(1, -1).long()
-        self.input_proj = nn.Conv1d(backbone.num_channels, hidden_dim, kernel_size=1)
-        self.query_embed = nn.Embedding(num_class, hidden_dim)
-        self.fc = GroupWiseLinear(num_class, hidden_dim, bias=True)
-    def forward(self, input,fea_pos=False):
-        input=rearrange(input,'b n c l -> (b n) c l')
-        src = self.backbone(input)
-        src=self.input_proj(src)
-        if fea_pos:
-            src+=self.feature_pos_encoding
-        hs = self.transformer(src)
-        return hs
-
+from cage.model import build_backbone,build_transformer,Tranmodel
 class finetunemodel(nn.Module):
     def __init__(self,pretrain_model,hidden_dim,bins,finetune_model='lstm'):
         super().__init__()
@@ -98,28 +54,20 @@ class finetunemodel(nn.Module):
             x=self.fc3(x)
         return x
 
-def build_backbone(args):
-    model = CNN(args.num_class, args.seq_length, args.embedsize)
-    return model
-def build_transformer(args):
-    return Transformer(
-        d_model=args.hidden_dim,
-        dropout=args.dropout,
-        nhead=args.nheads,
-        dim_feedforward=args.dim_feedforward,
-        num_encoder_layers=args.enc_layers,
-        num_decoder_layers=args.dec_layers
-    )
 def build_pre_train_model(args):
-    backbone = build_backbone(args)
+    backbone = build_backbone()
     transformer = build_transformer(args)
     pretrain_model = Tranmodel(
             backbone=backbone,
             transfomer=transformer,
-            num_class=args.num_class,
         )
-    if args.pretrain_path!='none':
-        pretrain_model.load_state_dict(torch.load(args.pretrain_path, map_location='cpu'))
+    if args.pretrain_path != 'none':
+        print('load pre-training model: ' + args.pretrain_path)
+        model_dict = pretrain_model.state_dict()
+        pretrain_dict = torch.load(args.pretrain_path, map_location='cpu')
+        pretrain_dict = {k: v for k, v in pretrain_dict.items() if k in model_dict}
+        model_dict.update(pretrain_dict)
+        pretrain_model.load_state_dict(model_dict)
     if not args.fine_tune:
         for param in pretrain_model.parameters():
             param.requires_grad = False
